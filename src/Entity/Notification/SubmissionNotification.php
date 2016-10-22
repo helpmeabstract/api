f408d82c87b777e9e4956e2782447253f1f9962c
Graham Daniels <greydnls@gmail.com>
2016-10-22 00:04:25 -0400

    Complete entities

--- src/Entity/Notification/SubmissionNotification.php
+++ src/Entity/Notification/SubmissionNotification.php
@@ -4,10 +4,15 @@ namespace HelpMeAbstract\Entity\Notification;
 
 use Doctrine\ORM\Mapping as ORM;
 use HelpMeAbstract\Entity\Notification;
+use HelpMeAbstract\Entity\Submission;
 
 /**
  * @ORM\Entity()
  */
 class SubmissionNotification extends Notification
 {
+    /**
+     * @var Submission
+     */
+    protected $subject;
 }
