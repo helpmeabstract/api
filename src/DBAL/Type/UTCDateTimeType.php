24c1532640c392134ff983028cd889a2b9d107ce
Graham Daniels <greydnls@gmail.com>
2016-11-14 21:49:25 -0500

    UTC FTW BB

---
+++ src/DBAL/Type/UTCDateTimeType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace HelpMeAbstract\DBAL\Type;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeType;
+
+class UTCDateTimeType extends DateTimeType
+{
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value instanceof \DateTime) {
+            $value->setTimezone(new \DateTimeZone('UTC'));
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (null === $value || $value instanceof \DateTime) {
+            return $value;
+        }
+
+        $converted = \DateTime::createFromFormat(
+            $platform->getDateTimeFormatString(),
+            $value,
+            new \DateTimeZone('UTC')
+        );
+
+        if (! $converted) {
+            throw ConversionException::conversionFailedFormat(
+                $value,
+                $this->getName(),
+                $platform->getDateTimeFormatString()
+            );
+        }
+
+        return $converted;
+    }
+}
